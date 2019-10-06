89badcabdad99e6549a6545fcbad8655890ba7fa
tqk <1282995880@qq.com>
2019-10-07 00:12:51 +0800

    持续集成测试 version1

---
+++ server/src/main/java/org/uppower/project/cashiermanagesystem/controller/CiController.java
@@ -0,0 +1,35 @@
+package org.uppower.project.cashiermanagesystem.controller;
+
+import cn.windyrjc.utils.response.Response;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ *
+ * @author ：涂齐康
+ * @date ：Created in 2019/10/7 12:10 上午
+ * @description：
+ * @modified By：
+ * @version:
+ */
+@RequestMapping("/ci")
+@RestController
+@Api(value = "测试ci",description = "持续集成")
+public class CiController {
+
+    @GetMapping("/out")
+    public Response<String> out(){
+        return Response.success("持续集成测试：version1");
+    }
+}
